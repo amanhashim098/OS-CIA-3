cc5faf4facd6123483022db0d98ef7923e8e9721
Aman Hashim <74784343+amanhashim098@users.noreply.github.com>
2024-09-28 10:31:56 +0530

    Update GameBoard.jsx

--- oscia/src/components/GameBoard.jsx
+++ oscia/src/components/GameBoard.jsx
@@ -43,7 +43,6 @@ export const GameBoard = () => {
           </div>
         ))}
       </div>
-  #HI ARUN
       <div className={styles.burstTimes}>
         {processes.map((process, index) => (
           <div key={index} className={styles.burstTime}>
